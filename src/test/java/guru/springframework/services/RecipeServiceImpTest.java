a4c4fcf2e784bd11f44b72ef4c9519cd30e1b126
wzh13 <wzh13@DESKTOP-FUVD5LD.home>
2019-10-11 09:25:07 -0700

    add show page

--- src/test/java/guru/springframework/services/RecipeServiceImpTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImpTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.services;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Before;
@@ -30,6 +32,21 @@ public class RecipeServiceImpTest {
 		recipeService = new RecipeServiceImp(recipeRepository);
 	}
 	
+    @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+    
 	@Test
 	public void getRecipes() throws Exception {
 		Recipe recipe = new Recipe();
